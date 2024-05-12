1b167885069b84f0ddad4384ad60c46d0af2d1b9
Tanzim <tanzimhossain2@gmail.com>
2024-05-12 11:28:34 +0600

    x

--- components/hotel/details/Summary.tsx
+++ components/hotel/details/Summary.tsx
@@ -4,7 +4,8 @@ const Summary = () => {
   return (
     <section className="py-4 mt-[100px] ">
       <div className="flex container">
-        <HotelSummaryInfo source="details"/>
+        {/* <HotelSummaryInfo source="details"/> */}
+         <HotelSummaryInfo fromListPage={false}/>
       </div>
     </section>
   );
